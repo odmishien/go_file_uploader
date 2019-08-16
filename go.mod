56ab8dc6d250c6a97fe20d50797450d21b82da6c
odmishien <tmtm.yagi.007@gmail.com>
2019-08-16 20:40:17 +0900

    upload s3

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_file_uploader
 
 go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.23.1
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+)
--- main.go
+++ main.go
@@ -6,11 +6,55 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
+
+func GetS3UploadManager() *s3manager.Uploader {
+	var AWS_ACCESS_KEY = os.Getenv("AWS_ACCESS_KEY")
+	var AWS_SECRET_ACCESS_KEY = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	creds := credentials.NewStaticCredentials(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, "")
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: creds,
+		Region:      aws.String("us-east-2")},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s3manager.NewUploader(sess)
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	tmpl.Execute(w, "")
+}
+
+func UploadToS3(file io.Reader, fileName string) error {
+	bucket := "odmishienbucket"
+	uploader := GetS3UploadManager()
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fileName),
+		Body:   file,
+	})
+	if err != nil {
+		log.Fatal("upload s3: ", err)
+		return err
+	}
+	return nil
+}
+
+func MakeTempFile(file io.Reader, fileName string) error {
+	f, err := os.OpenFile("imgs/"+fileName, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	return nil
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,12 +80,18 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer file.Close()
-	f, err := os.OpenFile("imgs/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+
+	err = MakeTempFile(file, handler.Filename)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
 	}
-	defer f.Close()
-	io.Copy(f, file)
+	tf, err := os.Open("imgs/" + handler.Filename)
+	err = UploadToS3(tf, handler.Filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	img := Image{
 		FileName: handler.Filename,
